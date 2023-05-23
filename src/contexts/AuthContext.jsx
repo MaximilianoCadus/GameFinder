7c20c302cade1a17a301c1f6d79ec96f8eddc4ae
Maximiliano Cadús <maxicadus172@gmail.com>
2023-05-23 13:40:11 -0300

    Help with the log in

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -7,72 +7,70 @@ import { SnackbarContext } from "./SnackbarContext";
 export const AuthContext = createContext();
 
 export const AuthContextProvider = (props) => {
-    const navigate = useNavigate();
-    const { setSnackbar } = useContext(SnackbarContext);
+  const navigate = useNavigate();
+  const { setSnackbar } = useContext(SnackbarContext);
 
-    const authUser = (user) => {
-        fetch("http://localhost:3000/login", {
-            method: "POST",
-            body: JSON.stringify({
-                email: user.mail,
-                password: user.password,
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8",
-            },
-        })
-            .then((response) => response.json())
-            .then((result) => {
-                if (result.hasOwnProperty("accessToken")) {
-                    localStorage.setItem(
-                        "jwt",
-                        JSON.stringify(result.accessToken)
-                    );
-                    localStorage.setItem("id", result.user.id);
-                    navigate("/home");
-                } else {
-                    setInputClass("input-wrong");
-                    setToggleClass("toggle-password-wrong");
-                    setSnackbar("Incorrect email or password.", "active");
-                }
-            })
-            .catch(() => {
-                setSnackbar("Oops! Something went wrong.", "active");
-            });
-    };
-
-    const [state, setState] = useState();
-    const [passwordType, setPasswordType] = useState("password");
-    const [inputClass, setInputClass] = useState("input");
-    const [toggleClass, setToggleClass] = useState("toggle-password");
-    const [eyeSrc, setEyeSrc] = useState(EyeSvg);
-
-    const passToggle = () => {
-        if (state) {
-            setPasswordType("password");
-            setState(false);
-            setEyeSrc(EyeSvg);
+  const authUser = (user) => {
+    fetch("http://localhost:3000/login", {
+      method: "POST",
+      body: JSON.stringify({
+        email: user.mail,
+        password: user.password,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.hasOwnProperty("accessToken")) {
+          localStorage.setItem("jwt", JSON.stringify(result.accessToken));
+          localStorage.setItem("id", result.user.id);
+          navigate("/home");
         } else {
-            setPasswordType("text");
-            setState(true);
-            setEyeSrc(NoEyeSvg);
+          alert("Try with user: altimetrik@mail.com password: 12345");
+          setInputClass("input-wrong");
+          setToggleClass("toggle-password-wrong");
+          setSnackbar("Incorrect email or password.", "active");
         }
-    };
+      })
+      .catch(() => {
+        setSnackbar("Oops! Something went wrong.", "active");
+      });
+  };
 
-    return (
-        <AuthContext.Provider
-            value={{
-                authUser,
-                passToggle,
-                setInputClass,
-                setToggleClass,
-                passwordType,
-                inputClass,
-                toggleClass,
-                eyeSrc,
-            }}
-        >
-            {props.children}
-        </AuthContext.Provider>
-    );
+  const [state, setState] = useState();
+  const [passwordType, setPasswordType] = useState("password");
+  const [inputClass, setInputClass] = useState("input");
+  const [toggleClass, setToggleClass] = useState("toggle-password");
+  const [eyeSrc, setEyeSrc] = useState(EyeSvg);
+
+  const passToggle = () => {
+    if (state) {
+      setPasswordType("password");
+      setState(false);
+      setEyeSrc(EyeSvg);
+    } else {
+      setPasswordType("text");
+      setState(true);
+      setEyeSrc(NoEyeSvg);
+    }
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        authUser,
+        passToggle,
+        setInputClass,
+        setToggleClass,
+        passwordType,
+        inputClass,
+        toggleClass,
+        eyeSrc,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
 };
